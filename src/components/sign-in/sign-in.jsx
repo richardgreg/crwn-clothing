b085ec1c186339ea11ff6c390576defea9ae0307
Richard O. Gregory <richardgrecoson@gmail.com>
2019-12-20 13:24:01 +0100

    Ensure sign-in with email and password authenticates.

--- src/components/sign-in/sign-in.jsx
+++ src/components/sign-in/sign-in.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import FormInput from "../form-input/form-input";
 import CustomButton from "../custom-button/custom-button";
 
-import {signInWithGoogle} from "../../firebase/firebase.utils";
+import {auth, signInWithGoogle} from "../../firebase/firebase.utils";
 
 import "./sign-in.scss";
 
@@ -16,9 +16,18 @@ class SignIn extends React.Component {
       password: ""
     }  
   }
-  handleSubmit = (event) => {
-    event.preventDefualt();
+  handleSubmit = async event => {
+    event.preventDefault();
 
+    const {email, password} = this.state;
+
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({email:"", password:""});
+    } catch (error) {
+      console.log(error);
+      
+    }
     this.setState({ email: "", password: "" })
   }
 
