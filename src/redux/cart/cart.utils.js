55db20957f342b40b39fd0d79010f183ecf15ee3
Richard O. Gregory <richardgrecoson@gmail.com>
2020-01-13 12:02:14 +0100

    Create cart-item component.
    Also implement a functionality that pulls shop items into the componentand display them.

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,10 +1,13 @@
 // any utils related to cart redux
 // Lesson: Add multiple items to cart
 export const addItemToCart = (cartItems, cartItemToAdd) => {
+  // Check for existing cart item
   const existingCartItem = cartItems.find(
     cartItem => cartItem.id === cartItemToAdd.id
     );
-
+  
+  // If item exists, add quantity and return a new array so component
+  // re-renders, else just return cary item
   if (existingCartItem) {
     return cartItems.map(cartItem =>
       cartItem.id === cartItemToAdd.id
@@ -12,5 +15,7 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
         : cartItem
       )
   }
+  // Return a neww array if item does not exist with base quant of 1
+  // Subsequent cart items can reference the quant
   return [...cartItems, {...cartItemToAdd, quantity: 1}];
 }
