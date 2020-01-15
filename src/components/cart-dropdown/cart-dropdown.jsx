bcfcbde7deb05c2279579bce28665384ecb0ec1b
Richard O. Gregory <richardgrecoson@gmail.com>
2020-01-15 18:01:31 +0100

    Make cart hidden when on checkout page.
    The dispatch action shorthand was introduced at this stage.

--- src/components/cart-dropdown/cart-dropdown.jsx
+++ src/components/cart-dropdown/cart-dropdown.jsx
@@ -6,10 +6,11 @@ import {withRouter} from "react-router-dom";
 import CustomButton from "../custom-button/custom-button";
 import CartItem from "../cart-item/cart-item";
 import { selectCartItems } from "../../redux/cart/cart.selectors";
+import {toggleCartHidden} from "../../redux/cart/cart.actions";
 
 import "./cart-dropdown.scss";
 
-const CartDropdown = ({cartItems, history}) => (
+const CartDropdown = ({cartItems, history, dispatch}) => (
   <div className="cart-dropdown">
     <div className="cart-items">
       {cartItems.length ?
@@ -20,9 +21,14 @@ const CartDropdown = ({cartItems, history}) => (
         <span className="empty-message">Cart is empty</span>
       }
     </div>
-    <CustomButton onClick={() => history.push("/checkout")}>GO TO CHECKOUT</CustomButton>
+    <CustomButton
+    onClick={() => {
+      history.push("/checkout");
+      dispatch(toggleCartHidden());
+    }}>
+    GO TO CHECKOUT</CustomButton>
   </div>
-)
+);
 
 const mapStateToProps = createStructuredSelector ({
   cartItems: selectCartItems
