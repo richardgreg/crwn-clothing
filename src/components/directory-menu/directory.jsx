863a95092b7dcda050c419ab87fb20e6411a6546
Richard O. Gregory <richardgrecoson@gmail.com>
2020-01-23 14:19:09 +0100

    Move directory and collection data into Redux.
    Create a folder for directory and shop data. Although they are
    used by single components, this will be beneficial when testing.

--- src/components/directory-menu/directory.jsx
+++ src/components/directory-menu/directory.jsx
@@ -1,27 +1,23 @@
 import React from "react";
+import {connect} from "react-redux";
+import {createStructuredSelector} from "reselect";
+
+import { selectDirectorySections } from "../../redux/directory/directoty.selector";
 
 import MenuItem from "../menu-item/menu-item";
 
-import DIRECTORY_DATA from "./directory-data";
 import "./directory-menu.scss"
 
-class Directory extends React.Component {
-  constructor(){
-    super();
+const Directory = ({sections}) => (
+  <div className='directory-menu'>
+    {sections.map(({id, ...otherSectionProps}) => (
+      <MenuItem key={id} {...otherSectionProps} />
+    ))}
+  </div>
+);
 
-    this.state = {
-      sections: DIRECTORY_DATA
-    }
-  }
-  render(){
-    return(
-      <div className='directory-menu'>
-        {this.state.sections.map(({id, ...otherSectionProps}) => (
-          <MenuItem key={id} {...otherSectionProps} />
-        ))}
-      </div>
-    );
-  }
-}
+const mapStateToProps = createStructuredSelector({
+  sections: selectDirectorySections
+});
 
-export default Directory;
+export default connect(mapStateToProps)(Directory);
