0af10547b8f51da844909a5567823a2cfc6d8f98
Richard O. Gregory <richardgrecoson@gmail.com>
2020-02-24 14:09:42 +0100

    Use useEffect React Hook for rendering components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect} from "react";
 import {Switch, Route, Redirect} from "react-router-dom";
 import {connect} from "react-redux";
 import { createStructuredSelector } from "reselect";
@@ -14,38 +14,43 @@ import CheckoutPage from "./pages/checkout/checkout.jsx";
 import {selectCurrentUser} from "./redux/user/user.selector";
 import {CheckUserSession} from "./redux/user/user.action";
 
-class App extends React.Component {
+const App = ({ checkUserSession, currentUser}) => {
+  // Depreciated! App now uses Hooks (useEffect)
   // Close subcription when auth.onAuthStateChanged
   // unmounts in order to prevent memory leaks
-  unsubscribeFromAuth = null;
+  // const unsubscribeFromAuth = null;
 
-  // Store user data/state in App
-  componentDidMount() {
-    const {checkUserSession} = this.props;
-    checkUserSession()
-  }
+  // This behaves like component did mount. Fire checkUser session once
+  // If there is a higher component than App, this effect can fire multiple
+  // times
+  useEffect(() => {
+      checkUserSession();
+    }, [checkUserSession]);
 
-  // Firebase: Unsubscribe from auth sevice when components
+  // Depreciated! Previously used to store user data/state in App
+  // componentDidMount() {
+  //   checkUserSession()
+  // }
+
+  // Depreciated! Firebase: Unsubscribe from auth sevice when components
   // unmounts
-  componentWillUnmount(){
-    this.unsubscribeFromAuth();
-  }
+  // componentWillUnmount(){
+  //   this.unsubscribeFromAuth();
+  // }
 
-  render() {
     return (
       <div>
-        <Header currentUser={this.props.currentUser}/>
+        <Header currentUser={currentUser}/>
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
           <Route exact path="/checkout" component={CheckoutPage} />
           <Route exact path="/signin"
-            render={() => this.props.currentUser ? (<Redirect to="/" />) : (<SignInAndSignUpPage />)}
+            render={() => currentUser ? (<Redirect to="/" />) : (<SignInAndSignUpPage />)}
           />
         </Switch>
       </div>
     );
-  }
 }
 
 // Get user state for redirect
--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Route} from "react-router-dom";
 import {connect} from "react-redux";
 
@@ -11,28 +11,23 @@ import {fetchCollectionsStart} from "../../redux/shop/shop.action";
 
  // Shop page is nested in a route. Shop page automatically
  // passes location, history and match as props. Destr. match.
-class ShopPage extends React.Component {
-  componentDidMount() {
-    const {fetchCollectionsStart} = this.props;
-
-    fetchCollectionsStart();
-  }
-
-  render(){
-    const { match} = this.props;
-    return(
-      // render is a method that takes a fxn where the params in the fxn are
-      // simply the params that the component will recieve which are the match,
-      // location and history props that route passes into the components which 
-      // gives collection page access to match that we use in selector
-      <div className='shop-page'>
-        <Route exact path={`${match.path}`} component={CollectionsOverviewContainer} />
-        <Route path={`${match.path}/:collectionId`} component={CollectionPageContainer} />
-      </div>
-    );
-  }
-}
-
+const ShopPage = ({match, fetchCollectionsStart}) => {
+ // Pass in fetchCollectionsStart as secondary argument so useEffect
+ // does not rerender when a precursor fires.
+  useEffect(() => {
+   fetchCollectionsStart();
+ }, [fetchCollectionsStart]);
+  return(
+    // render is a method that takes a fxn where the params in the fxn are
+    // simply the params that the component will recieve which are the match,
+    // location and history props that route passes into the components which 
+    // gives collection page access to match that we use in selector
+    <div className='shop-page'>
+      <Route exact path={`${match.path}`} component={CollectionsOverviewContainer} />
+      <Route path={`${match.path}/:collectionId`} component={CollectionPageContainer} />
+    </div>
+  );
+};
 
 const mapDispatchToProps = dispatch => ({
   fetchCollectionsStart: () => dispatch(fetchCollectionsStart())
